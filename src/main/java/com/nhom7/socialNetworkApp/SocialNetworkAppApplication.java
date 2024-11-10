e219c4f2c5e98067fd78e154ef2956948230f915
pchien925 <pcchien250904@gmail.com>
2024-11-10 23:24:51 +0700

    init new social network app

---
+++ src/main/java/com/nhom7/socialNetworkApp/SocialNetworkAppApplication.java
@@ -0,0 +1,13 @@
+package com.nhom7.socialNetworkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhom7/socialNetworkApp/SocialNetworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom7.socialNetworkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
